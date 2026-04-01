b784d0da8de199a0db6a8e7eb4213158eb98d8df
Andriy Adamovych <andriy.adamovych@contracted.pmi.com>
2026-04-01 22:48:26 +0200

    chore: added searching coutry answer

--- get_country/usercode.js
+++ get_country/usercode.js
@@ -1,19 +1,28 @@
 const fs = require("fs");
 
 module.exports = async function (data) {
-    try {
-        const json = JSON.parse(fs.readFileSync('./countries.json'));
+  try {
+    const json = JSON.parse(fs.readFileSync("./countries.json"));
 
-        const result = json.countries.filter(p => (p.name.charAt(0).toLowerCase() === data.letter.toLowerCase()));
-        return {
-            "result": "ok",
-            "countries": result
-        };
+    const result = json.countries.filter(
+      (p) => p.name.charAt(0).toLowerCase() === data.letter.toLowerCase(),
+    );
 
-    } catch (e) {
-        return {
-            "result": "error",
-            "errorMessage": e.message
-        };
-    }
+    if (result.length === 0)
+      return {
+        result: "not_found",
+        countries: [],
+      };
+    else
+      return {
+        result: "ok",
+        countries: result,
+      };
+      
+  } catch (e) {
+    return {
+      result: "error",
+      errorMessage: e.message,
+    };
+  }
 };
